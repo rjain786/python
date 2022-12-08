a303e95a85f4b9d0a8f6bf22bcd60ec9f8bf733d
rjain786 <440530@edu.cwa.ac.uk>
2022-12-08 12:17:51 +0000

    calc

---
+++ Unit02/calc.py
@@ -0,0 +1,14 @@
+newlist = []
+max_digit = int(input("how many digits  in a number to add to a list: "))
+
+x = 0
+while x <= max_digit:
+    print(int(input("Enter an integer: ")))
+    x +=1
+
+choose =  input("Enter 'm' for multiply and 'a' for addition:  ")
+
+    
+
+
+
