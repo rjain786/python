66ff5428d3e620c7147b21e3196099a84cc46fd3
rjain786 <440530@edu.cwa.ac.uk>
2022-12-08 15:15:13 +0000

    calc

--- Unit02/calc.py
+++ Unit02/calc.py
@@ -1,12 +1,36 @@
-newlist = []
+new_list = []
 max_digit = int(input("how many digits  in a number to add to a list: "))
 
-x = 0
-while x <= max_digit:
-    print(int(input("Enter an integer: ")))
-    x +=1
 
-choose =  input("Enter 'm' for multiply and 'a' for addition:  ")
+while max_digit > 0:
+    num = input("Enter an integer: ")
+    if num.isdigit():
+        new_list.append(int(num))
+            
+    else:
+        print("invalid input, not counted")
+        
+    max_digit -= 1
+
+print("done")
+print("Your new list is:", new_list)
+
+
+choose =  input("Enter 'm' for multiply and 'a' for addition:  ").lower()
+sum = 0
+multiply = 1
+if choose == "m":
+    for num in new_list:
+        multiply = multiply * num
+    print("multiplication of the numbers are: ", multiply)
+elif choose == "a":
+    for num in new_list:
+        sum += num
+    print("addition of the numbers are: ", sum)
+else:
+    print("invalid choice")
+
+print("all done, see you again soon!")
 
     
 
